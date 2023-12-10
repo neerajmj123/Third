48e57b0365fe1d0c536c0d7ce74e151ecad76e8f
neerajmj <nmj41198@gmail.com>
2023-12-10 21:00:09 +0530

    calc

--- 8-12-23/calcu.js
+++ 8-12-23/calcu.js
@@ -2,3 +2,11 @@ function buttonclick(val)
 {
     document.getElementById("inp").value+=val;
 }
+function buttonclear(){
+    document.getElementById("inp").value="";
+}
+function buttonclickequal(){
+    var text=document.getElementById("inp").value
+    var result=eval(text)
+    document.getElementById("inp").value=result
+}
