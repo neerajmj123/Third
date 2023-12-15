522b890df7f02754836f5e9824406c5a260404b3
neerajmj <nmj41198@gmail.com>
2023-12-16 00:47:12 +0530

    java16

---
+++ new site/java.js
@@ -0,0 +1,24 @@
+let xhr = new XMLHttpRequest()
+
+xhr.open("get",'https://fakestoreapi.com/products')
+xhr.send()
+console.log("xhr",xhr)
+xhr.onreadystatechange = function(){
+    let items =document.getElementById('items')
+    let response = xhr.response
+    console.log("response",response)
+    let parced_response = JSON.parse(response)
+    console.log("parsed_response",parced_response)
+     datas=''
+    for(let i=0;i<parced_response.length;i++){
+         datas=datas+`
+         <div class="card1">
+        <h3 class="id">${parced_response[i].id}</h3>
+        <h3 class="title">${parced_response[i].title}</h3>
+        <p class="des">${parced_response[i].description}</p>
+        <img src="${parced_response[i].image}" alt="">
+        <h5 class="rat">${parced_response[i].rating}</h5>
+       </div>`
+            }
+            items.innerHTML=datas
+}
