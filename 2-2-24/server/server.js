9404cc193768228bf2e4fb0ca57405dab0b7802b
neerajmj <nmj41198@gmail.com>
2024-01-02 16:05:10 +0530

    mongodb

---
+++ 2-2-24/server/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs')
+const queryString =require('querystring');
+const {MongoClient} = require('mongodb')
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const client = new MongoClient("mongodb://127.0.0.1:27017");
+
+const server = http.createServer((req, res) => {
+
+    console.log("url : ",req.url);
+
+    const parsed_url= url.parse(req,url)
+    console.log("parsed_url :",parsed_url);
+    
+    if(parsed_url.pathname === "/"){
+        res.writeHead(200,{'Content-Type ': 'text/html'})
+        res.end(fs.readFileSync('../client/inex.html'))
+    }else if (parsed_url.pathname === '../style.css'){
+    res.writeHead(200,{'Content-Type' : "text/css"})
+    res.end(fs.readFileSync("../client/style.css"))
+    }
+
+    if(req.method === "POST" && parsed_url.pathname === "/submit"){
+        console.log("Form Submitted Sucessfully")
+        let body = '';
+
+        req.on('data',(chunk)=>{
+            console.log("chunk : ",chunk);
+            console.log('chunk.toSring() :',chunk.toString())
+            body+=chunk.toString();
+            console.log("body :",body)
+        })
+        
+    }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
