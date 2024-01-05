8701abaa854384e24855625f2ecd73735b33db2b
neerajmj <nmj41198@gmail.com>
2024-01-05 15:56:10 +0530

    mongodb5

--- 2-2-24/client/script.js
+++ 2-2-24/client/script.js
@@ -1,4 +1,19 @@
+console.log("hai")
 async function getuserData(){
     let userData = await fetch ('http://localhost:3000/getData')
-    console.log("userData")
+    console.log("userData",userData)
+    let parseduserData =await userData.json()
+    console.log("parsedUserdata",parseduserData)
+    let tbody = document.getElementById('tbody')
+    let content=''
+    for(let i=0;i<parseduserData.length;i++){
+        content=content+`
+        <tr>
+        <td>${parseduserData[i].name}</td>
+        <td>${parseduserData[i].email}</td>
+        <td>${parseduserData[i].password}</td>
+        <tr>`
+    }
+    tbody.innerHTML=content
 }
+getuserData();
--- 2-2-24/server/server.js
+++ 2-2-24/server/server.js
@@ -8,83 +8,85 @@ let hostname = '127.0.0.1';
 const port = 3000;
 
 const client = new MongoClient("mongodb://127.0.0.1:27017")
-const server = http.createServer((req,res) => {
+const server = http.createServer(async (req, res) => {
 
     const db = client.db("users")
     const collection = db.collection("users_coll")
 
-    console.log("url : ",req.url);
+    console.log("url : ", req.url);
     const parsed_url = url.parse(req.url)
-    console.log("parsed_url :",parsed_url);
-    
-    if(parsed_url.pathname ==='/'){
-        res.writeHead(200,{'Content-Type': 'text/html'})
-        res.end(fs.readFileSync('../client/inex.html'))
-    }else if (parsed_url.pathname === '/style.css'){
-    res.writeHead(200,{'Content-Type' : "text/css"})
-    res.end(fs.readFileSync("../client/style.css"))
-    }else if (parsed_url.pathname === '/add_user.html'){
-    res.writeHead(200,{'Content-Type' : "text/html"})
-    res.end(fs.readFileSync("../client/add_user.html"))
-    }else if (parsed_url.pathname === '/view_user.html'){
-    res.writeHead(200,{'Content-Type' : "text/html"})
-    res.end(fs.readFileSync("../client/view_user.html"))
-    }
+    console.log("parsed_url :", parsed_url);
 
-    if(req.method === "POST" && parsed_url.pathname === "/submit"){
+    if (parsed_url.pathname === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html'})
+        res.end(fs.readFileSync('../client/inex.html'))
+    } else if (parsed_url.pathname === '/style.css') {
+        res.writeHead(200, { 'Content-Type': "text/css"})
+        res.end(fs.readFileSync("../client/style.css"))
+    } else if (parsed_url.pathname === '/adduser.html') {
+        res.writeHead(200, { 'Content-Type': "text/html" })
+        res.end(fs.readFileSync("../client/adduser.html"))
+    } else if (parsed_url.pathname === '/view_user.html') {
+        res.writeHead(200, { 'Content-Type': "text/html" })
+        res.end(fs.readFileSync("../client/view_user.html"))
+    } else if (parsed_url.pathname === '/script.js') {
+        res.writeHead(200, { 'Content-Type': "text/javascript"})
+        res.end(fs.readFileSync("../client/script.js"))
+    }
+    if (req.method === "POST" && parsed_url.pathname === "/submit") {
         console.log("Form Submitted Sucessfully")
         let body = '';
 
-        req.on('data',(chunk)=> {
-            console.log("chunk : ",chunk);
-            console.log('chunk.toSring() : ',chunk.toString())
-            body+=chunk.toString();
-            console.log("body :",body)
+        req.on('data', (chunk) => {
+            console.log("chunk : ", chunk);
+            console.log('chunk.toSring() : ', chunk.toString())
+            body += chunk.toString();
+            console.log("body :", body)
         })
-        req.on('end',async ()=> {
+        req.on('end', async () => {
             const formData = queryString.parse(body)
-            console.log("formData :",formData)
+            console.log("formData :", formData)
 
-            console.log("name :",formData.name)
-            console.log("email ",formData.email)
-            console.log("password",formData.password)
+            console.log("name :", formData.name)
+            console.log("email ", formData.email)
+            console.log("password", formData.password)
 
             collection.insertOne(formData)
-            .then((message) => {
-                console.log("document saved succesfully")
-                console.log("message", message)
-            })
-            .catch((error) => {
-                console.log("document not inserted ")
-                console.log("database insertion error", error)
-            })
-            res.writeHead(200,{'Content-Type':"text/plain"})
+                .then((message) => {
+                    console.log("document saved succesfully")
+                    console.log("message", message)
+                })
+                .catch((error) => {
+                    console.log("document not inserted ")
+                    console.log("database insertion error", error)
+                })
+            res.writeHead(200, { 'Content-Type': "text/plain" })
             res.end("form submitted sucessfully")
         })
-        
-    }
-    if(req.method === "GET" && parsed_url.pathname ==="/getdata"){
-        let data = await collection.find().toArray()
-        console.log("data :",data)
-        let json_data = JSON.stringify(data)
-        console.log("json_data",json_data)
 
-        res.writeHead(200,{"Content_Type":"text/json"})
+    }
+    if (req.method === "GET" && parsed_url.pathname === "/getData") {
+        let data = await collection.find().toArray();
+        console.log("data :", data)
+        let json_data = JSON.stringify(data)
+        console.log("json_data", json_data)
+
+        res.writeHead(200, { "Content_Type": "text/json" })
         res.end(json_data)
     }
 });
-async function connect(){
- await client.connect()
- .then((message)=>{
-    console.log("database connection established")
- })
- .catch((error)=>{
-    console.log("database connection error ",error)
- })
- .finally(()=>{
-    server.listen(port, () => {
-        console.log(`Server running at http://127.0.0.1:${port}/`);
-      });
- })
+async function connect() {
+    await client.connect()
+        .then((message) => {
+            console.log("database connection established")
+        })
+        .catch((error) => {
+            console.log("database connection error ", error)
+        })
+        .finally(() => {
+            server.listen(port, () => {
+                console.log(`Server running at http://localhost:${port}/`);
+            });
+        })
 }
 connect();
