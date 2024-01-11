78c795358d1c4af146cebaa6843fca55b530e544
neerajmj <nmj41198@gmail.com>
2024-01-11 11:30:46 +0530

    todolist11

--- todolist/server/server.js
+++ todolist/server/server.js
@@ -36,11 +36,37 @@ const server = http.createServer((req, res) => {
   req.on('end', async () => {
     const formData = queryString.parse(body)
     console.log("formData :", formData)
+
+    console.log("name",formData.tasks)
+
+    collection.insertOne(formData)
+    .then((message)=>{
+      console.log("document saved succesfully")
+      console.log("message",message)
+    })
+    .catch((error)=>{
+      console.log("document not inserted ")
+      console.log("databse insertion error",error)
+    })
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("task addded  succesfully")
   })
 }
   
 
 });
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+async function connect() {
+  await client.connect()
+      .then((message) => {
+          console.log("Database connection established")
+      })
+      .catch((error) => {
+          console.log("Database connection error ", error)
+      })
+      .finally(() => {
+          server.listen(port, () => {
+              console.log(`Server running at http://localhost:${port}/`);
+          });
+      })
+}
+connect();
