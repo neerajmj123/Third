9c19d5207b95ae6797aaf0fbfbdfeada6197b539
neerajmj <nmj41198@gmail.com>
2024-01-12 16:00:00 +0530

    express12

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ let port = process.env.PORT
 app.get('/test',(req,res)=>{
     res.status(200).send("sucess")
 })
+console.log("__dirname ",__dirname)
+app.use('/',express.static(__dirname+"/"../client))
 app.listen(port,()=>{
     console.log(`sever started at http://localhost:${port}`)
 })
