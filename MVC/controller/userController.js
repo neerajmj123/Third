9da1663b8f25e1de5b1bf8719aa02bf3aee6bbb2
neerajmj <nmj41198@gmail.com>
2024-02-01 10:42:00 +0530

    MVC1

--- MVC/controller/userController.js
+++ MVC/controller/userController.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const users = require('../db/models/users')
 const success_function = require('../util/response-handler').success_function;
 const error_function = require('../util/response-handler').error_function
@@ -43,12 +44,73 @@ exports.createUser = async function (req,res){
     }
 }
  exports.getuserData = async function (req,res){
-        const users = await model.find();
-        res.status(200).json(users)
+        try {
+            const user = await users.find()
+
+            if(user.length>0){
+                res.status(200).send(user)
+            }else{
+                res.status(400).send("user not found")
+            }
+        } catch (error) {
+            console.error("users canot be fetch",error)
+            res.status(400).send("data not found")exports
+        }
 }
-async function editData(){
+exports.editData = async function ( res,req){
+ let data = req.body
+ console.log("data",data)
+ let id = data.id
+ console.log("id",id)
+
+ let _id = new mongoose.Types.ObjectId(id)
+ console.log("_id",_id)
+
+ let updateData ={
+    name : data.name,
+    email :data.email,
+    password :data.password
+ }
+ const isUserExist = await users.findOne({email : data.email})
+ console.log("isuserExist",isUserExist)
+
+ if(isUserExist){
+    let response= error_function({
+        statusCode : 400;
+        message : "user already exist ",
+    })
+    res.status(response.statusCode).send(response)
+    return
+ }
+ await users.updateOne({_id},{$set:updateData})
+ .then((message)=>{
+    console.log("document updted sucessfully ",message)
+    res.status(200).send("document updtaes successfully")
+
+ })
+ .catch((error)=>{
+    console.log("document not updated ",error)
+    res.status(400).send("failed")
+ })
 
 }
-async function deleteData(){
+exports.deleteData =async function (req,res){
+let data = req.body;
+console.log("data",data)
 
+let id = data;
+console.log("id",id)
+
+let _id = new mongoose.Types.ObjectId(id)
+console.log("_id",_id)
+
+users.findByIdAndDelete(_id)
+.then((message)=>{
+    console.log("deleted sucessfully")
+    res.status(200).send("success")
+})
+.catch((error)=>{
+    console.log("delted succesfully")
+    res.status(200).send("failed")
+})
 }
--- MVC/routes/userRoutes.js
+++ MVC/routes/userRoutes.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const userController = require('../controller/userController');
 router.post('/submit',userController.createUser);
 router.get('/getData',userController.getuserData)
+router.get('/editData',userController.editData)
+router.get('/deleteData',userController.deleteData)
 module.exports = router;
